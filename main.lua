b67de12de34334e0c7f0e20c03de44813c37a831
Robert Burke <sharpobject@gmail.com>
2014-07-22 12:41:54 -0700

    Add some of the best cards

--- main.lua
+++ main.lua
@@ -5,5 +5,10 @@ function love.load()
 end
 
 function love.draw()
+  local win_w = love.graphics.getWidth()
+  local win_h = love.graphics.getHeight()
   love.graphics.draw(gfx.background)
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.line(0, win_h/2-1, win_w, win_h/2-1)
+  love.graphics.setColor(255, 255, 255)
 end
